9ea1db2438009745b15eeba57fbbb7cc4f59bda7
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-05-01 16:15:30 -0300

    Finalizado vídeo aula 6

---
+++ MeuPrimeiroTeste/MyClasses/FileProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace MyClasses
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (String.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("FileName is not valid.");
+            }
+
+            return File.Exists(fileName);
+        }
+    }
+}
---
+++ MeuPrimeiroTeste/MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MyClasses;
+using Xunit;
+
+namespace MyClassesTest
+{
+    public class Tests
+    {
+        [Fact]
+        public void FileNameExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Studyspace\unit-testing-course\Files\emBranco.txt");
+
+            Assert.True(fromCall);
+        }
+
+        [Fact]
+        public void FileNameDoesntExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Studyspace\unit-testing-course\Files\naoExiste.txt");
+
+            Assert.False(fromCall);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void WhenFileNameIsNullOrEmpty_ThrowArgumentNullException(string fileName)
+        {
+            FileProcess fp = new FileProcess();
+
+            Assert.Throws<ArgumentNullException>(() => fp.FileExists(fileName));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void WhenFileNameIsNullOrEmpty_ThrowArgumentNullException_UsingTryCatch(string fileName)
+        {
+            try
+            {
+                FileProcess fp = new FileProcess();
+
+                fp.FileExists(fileName);
+            }
+            catch (ArgumentNullException)
+            {
+                return;
+            }
+        }
+    }
+}
